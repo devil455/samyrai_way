a332d919c1d05e2d0638cdb0b79825c3d5da1c4c
Alex Astashonok <aleksei.astashonok@yandex.ru>
2022-05-18 01:39:18 +0300

    2

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () =>{
+    return(
+    <div className='content'>
+   <div>Content:</div> 
+   <img src="https://avatars.mds.yandex.net/i?id=d3e444f199fe261de3247784435402b5-5877259-images-thumbs&n=13"></img>
+   <div>ava + description</div>
+   <div>My posts</div>
+   <div>New posts</div>
+   <div>Posts</div>
+    </div>
+    )
+}
+
+export default Profile;
