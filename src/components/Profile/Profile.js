8d44a732344e61cb4583036e92ce06e72edc69a5
Alex Astashonok <aleksei.astashonok@yandex.ru>
2022-05-20 03:30:52 +0300

    20_05

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from './Profile.module.css'
+import Post from './Posts/Post/Post'
+
+const Profile = () =>{
+    return(
+    <div className={classes.content}>
+   <div>Content:</div> 
+   <img src="https://info.sibnet.ru/ni/560/560158w_1578630043.jpg"></img>
+   <div>ava + description</div>
+   <Post name = 'Alex' age = '30' massege ='Hi,how are you?' likesCount='12'/>
+   <Post name = 'Vit' age = '36' massege = 'Its ok!'/>
+   <Post name = 'Tem' age = '24' massege = 'Und You?'/>
+   <Post name = 'Alex' age = '30' massege = 'Im ok, too!'/>
+   <div>My posts</div>
+   <div>New posts</div>
+   <div>Posts</div>
+    </div>
+    
+    )
+}
+
+export default Profile;
